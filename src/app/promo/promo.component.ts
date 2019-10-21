11ff7a0bb54b0108eee85d13582f947da1085879
DonCorleone <vitocorleone77@gmail.com>
2019-10-21 14:32:58 +0200

    vegas in TS

--- src/app/promo/promo.component.ts
+++ src/app/promo/promo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+/// <reference path ="../../node_modules/@types/jquery/index.d.ts"/>
+declare var $: any;
 
 @Component({
   selector: 'app-promo',
@@ -11,6 +13,25 @@ export class PromoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    /* ======= Vegas Plugin ======= */
+    /* Ref: http://vegas.jaysalvat.com/index.html */
+    $('#promo').vegas({
+      delay: 8000,
+      overlay: 'assets/plugins/vegas/dist/overlays/06.png',
+      color: '#101113',
+      transition: 'zoomOut',
+      transitionDuration: 3000,
+      slides: [
+          { src: 'assets/images/hero/hero-1.jpg' },
+          { src: 'assets/images/hero/hero-2.jpg' },
+          { src: 'assets/images/hero/hero-3.jpg' },
+          { src: 'assets/images/hero/hero-4.jpg' },
+          { src: 'assets/images/hero/hero-5.jpg' },
+          { src: 'assets/images/hero/hero-6.jpg' },
+          { src: 'assets/images/hero/hero-7.jpg' },
+          { src: 'assets/images/hero/hero-8.jpg' }
+      ]
+  });
   }
 
 }
--- src/assets/plugins/main.js
+++ src/assets/plugins/main.js
@@ -44,25 +44,25 @@ $(document).ready(function() {
   //        }
   //   });
 
-	/* ======= Vegas Plugin ======= */
-    /* Ref: http://vegas.jaysalvat.com/index.html */
-    $('#promo').vegas({
-        delay: 8000,
-        overlay: 'assets/plugins/vegas/dist/overlays/06.png',
-        color: '#101113',
-        transition: 'zoomOut',
-        transitionDuration: 3000,
-        slides: [
-            { src: 'assets/images/hero/hero-1.jpg' },
-            { src: 'assets/images/hero/hero-2.jpg' },
-            { src: 'assets/images/hero/hero-3.jpg' },
-            { src: 'assets/images/hero/hero-4.jpg' },
-            { src: 'assets/images/hero/hero-5.jpg' },
-            { src: 'assets/images/hero/hero-6.jpg' },
-            { src: 'assets/images/hero/hero-7.jpg' },
-            { src: 'assets/images/hero/hero-8.jpg' }
-        ]
-    });
+	// /* ======= Vegas Plugin ======= */
+  //   /* Ref: http://vegas.jaysalvat.com/index.html */
+  //   $('promo', 'body').vegas({
+  //       delay: 8000,
+  //       overlay: 'assets/plugins/vegas/dist/overlays/06.png',
+  //       color: '#101113',
+  //       transition: 'zoomOut',
+  //       transitionDuration: 3000,
+  //       slides: [
+  //           { src: 'assets/images/hero/hero-1.jpg' },
+  //           { src: 'assets/images/hero/hero-2.jpg' },
+  //           { src: 'assets/images/hero/hero-3.jpg' },
+  //           { src: 'assets/images/hero/hero-4.jpg' },
+  //           { src: 'assets/images/hero/hero-5.jpg' },
+  //           { src: 'assets/images/hero/hero-6.jpg' },
+  //           { src: 'assets/images/hero/hero-7.jpg' },
+  //           { src: 'assets/images/hero/hero-8.jpg' }
+  //       ]
+  //   });
 
     /* ======= Countdown ========= */
 	// set the date we're counting down to
