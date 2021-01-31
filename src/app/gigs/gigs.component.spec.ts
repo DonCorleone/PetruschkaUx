9d5b56c0365f272791eff27a6fe9addeaa66eeda
Linus <vitocorleone77@gmail.com>
2021-01-31 23:25:23 +0100

    ng 11

--- src/app/gigs/gigs.component.spec.ts
+++ src/app/gigs/gigs.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GigsComponent } from './gigs.component';
 
@@ -6,7 +6,7 @@ describe('GigsComponent', () => {
   let component: GigsComponent;
   let fixture: ComponentFixture<GigsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GigsComponent ]
     })
