20bff3ee24f8242363d76ebea26bcd6cc1abbdc6
Linus <vitocorleone77@gmail.com>
2021-02-07 19:29:09 +0100

    refactoring components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScrollSpyDirective } from '../directives/scroll-spy.directive';
 import { HeaderComponent } from './header/header.component';
-import { MusicComponent } from './music/music.component';
-import { MerchComponent } from './merch/merch.component';
-import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { VideoComponent } from './video/video.component';
-import { InfoComponent } from './info/info.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CarouselUpdateComponent } from './carousel-update/carousel-update.component';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 
@@ -26,6 +20,12 @@ import { GigsItemComponent } from './components/gigs/gigs-item/gigs-item.compone
 import { AboutItemComponent } from './components/about/about-item/about-item.component';
 import { PromoComponent } from './components/promo/promo/promo.component';
 import { CarouselPromoComponent } from './components/promo/carousel-promo/carousel-promo.component';
+import { MusicComponent } from './components/music/music.component';
+import { CarouselUpdateComponent } from './components/carousel-update/carousel-update.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { InfoComponent } from './components/info/info.component';
+import { MerchComponent } from './components/merch/merch.component';
+import { VideoComponent } from './components/video/video.component';
 
 const uri = realm.graphqlUrl;
 
