fa33af82ee42e882d4fab4a2c8eda4dce9c01af6
Linus <vitocorleone77@gmail.com>
2021-02-21 21:47:59 +0100

    disable

--- scully.kinderkulturUx.config.ts
+++ scully.kinderkulturUx.config.ts
@@ -1,11 +1,11 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 import { HomeComponent } from 'src/app/home/home.component';
-const {MinifyHtml} = require('scully-plugin-minify-html');
-const {DisableAngular} = require('scully-plugin-disable-angular');
-setPluginConfig(DisableAngular, {
-  removeState: true
-});
-const postRenderers = [DisableAngular, MinifyHtml];
+// const {MinifyHtml} = require('scully-plugin-minify-html');
+// const {DisableAngular} = require('scully-plugin-disable-angular');
+// setPluginConfig(DisableAngular, {
+//   removeState: true
+// });
+// const postRenderers = [DisableAngular, MinifyHtml];
 
 const {
   RouteTypes,
@@ -13,13 +13,13 @@ const {
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
-  defaultPostRenderers: postRenderers,
+  // defaultPostRenderers: postRenderers,
   projectName: "kinderkulturUx",
   outDir: './dist/static',
   routes: {
-    '': {
-      type: RouteTypes.json,
-      url: 'http://localhost:4200/home'
-    }
+    // '': {
+    //   type: RouteTypes.json,
+    //   url: 'http://localhost:4200/home'
+   // }
   }
 };
