52b2d157414d0b5444388672acaa467cb21d00a3
Linus <vitocorleone77@gmail.com>
2021-03-19 13:18:24 +0100

    finetune

--- src/app/components/update/update-slide/update-slide.component.ts
+++ src/app/components/update/update-slide/update-slide.component.ts
@@ -30,10 +30,13 @@ export class UpdateSlideComponent implements AfterViewInit {
 		return EventService.GetPicBannerPathFromEventDetail(this.eventDetail);
 	}
 
+	get start(): Date {
+		return this.eventDetail.start;
+	}
 	ngAfterViewInit(): void {
 		/* ======= Countdown ========= */
-		// set the date we're counting down to
-		// const targetDate = new Date('July 18, 2021').getTime();
+		// // set the date we're counting down to
+		// const targetDate = new Date('July 08, 2021').getTime();
 
 		// // get tag element
 		// const countdown = document.getElementById('countdown-box');
@@ -58,34 +61,34 @@ export class UpdateSlideComponent implements AfterViewInit {
 			.scrollIntoView({behavior: 'smooth'});
 	}
 
-	refreshCountDown(targetDate: any, daysSpan: HTMLElement, hoursSpan: HTMLElement, minutesSpan: HTMLElement, secsSpan: HTMLElement) {
+	// refreshCountDown(targetDate: any, daysSpan: HTMLElement, hoursSpan: HTMLElement, minutesSpan: HTMLElement, secsSpan: HTMLElement) {
 
-		// variables for time units
-		let days;
-		let hours: number;
-		let minutes: number;
-		let seconds: number;
+	// 	// variables for time units
+	// 	let days;
+	// 	let hours: number;
+	// 	let minutes: number;
+	// 	let seconds: number;
 
-		// find the amount of "seconds" between now and target
-		const currentDate = new Date().getTime();
-		let secondsLeft = (targetDate - currentDate) / 1000;
+	// 	// find the amount of "seconds" between now and target
+	// 	const currentDate = new Date().getTime();
+	// 	let secondsLeft = (targetDate - currentDate) / 1000;
 
-		// do some time calculations
-		days = Math.round(secondsLeft / 86400);
-		secondsLeft = Math.round(secondsLeft % 86400);
+	// 	// do some time calculations
+	// 	days = Math.round(secondsLeft / 86400);
+	// 	secondsLeft = Math.round(secondsLeft % 86400);
 
-		hours = Math.round(secondsLeft / 3600);
-		secondsLeft = Math.round(secondsLeft % 3600);
+	// 	hours = Math.round(secondsLeft / 3600);
+	// 	secondsLeft = Math.round(secondsLeft % 3600);
 
-		minutes = Math.round(secondsLeft / 60);
-		seconds = Math.round(secondsLeft % 60);
+	// 	minutes = Math.round(secondsLeft / 60);
+	// 	seconds = Math.round(secondsLeft % 60);
 
-		// format countdown string + set tag value.
-		daysSpan.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Days</span>';
-		hoursSpan.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
-		minutesSpan.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
-		secsSpan.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Secs</span>';
-	}
+	// 	// format countdown string + set tag value.
+	// 	daysSpan.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Days</span>';
+	// 	hoursSpan.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
+	// 	minutesSpan.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
+	// 	secsSpan.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Secs</span>';
+	// }
 
 	openInfo(): void {
 		const modalRef = this.modalService.open(InfoModalComponent);
