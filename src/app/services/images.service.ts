e02bd1d07adca981d87e3db8916547e5940cb758
Linus <vitocorleone77@gmail.com>
2021-04-07 00:13:14 +0200

    image4

---
+++ src/app/services/images.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagesService {
+
+  constructor(private http:HttpClient) {
+
+	}
+
+	getAlbum(albumHash:String):Observable<Image4Response> {
+
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type':  'application/json',
+				'Authorization': 'Basic ' + btoa('BjEOISctoAxH7XBVaEDf0g==:frjjIdPvwRALz1PT22cONp43FZQLvRYGW6qtLc2tWOk=')
+			})
+		};
+
+		return this.http.get<Image4Response>(`https://api.image4.io/v0.1/listFolder?path=/test/`, httpOptions);
+	}
+}
+
+
+    export interface File {
+        folder: string;
+        orginal_name: string;
+        name: string;
+        size: number;
+        format: string;
+        width: number;
+        height: number;
+        createdAt: Date;
+        updatedAt: Date;
+    }
+
+    export interface Image4Response {
+        folders: any[];
+        files: File[];
+    }
+
+
