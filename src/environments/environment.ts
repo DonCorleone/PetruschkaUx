5bba7fe82d6f893f6f80806501de78b709fa2328
Linus <vitocorleone77@gmail.com>
2021-04-11 11:46:12 +0200

    env under scm

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+	production: false,
+// 	apiUrl: 'https://api.image4.io/v0.1',
+// 	apiKey: '4Yy9ZWYNl/HKYOwSIMI9LQ==',
+// 	apiSecret: 'FQw/fSWJTedUN36rXBBxAXsG+DKuM+Sz3xvk9m+IhRU='
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
