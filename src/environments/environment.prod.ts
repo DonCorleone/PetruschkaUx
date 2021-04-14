127ccf2b0f43f1bf1c37d9e3cbac69b9a05548cc
Linus <vitocorleone77@gmail.com>
2021-04-14 21:38:59 +0200

    another remove

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
 export const environment = {
-	production: true,
-	image4IoApiKey: process.env.API_KEY_IMAGE4IO,
-  image4IoApiSecret: process.env.API_SECRET_IMAGE4IO,
-  image4IoApiUrl: process.env.API_URL_IMAGE4IO,
-	realmAppId: process.env.APP_ID_REALM
+	production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-	image4IoApiKey: process.env.API_KEY_IMAGE4IO,
-  image4IoApiSecret: process.env.API_SECRET_IMAGE4IO,
-  image4IoApiUrl: process.env.API_URL_IMAGE4IO,
-	realmAppId: process.env.APP_ID_REALM
+  production: false
 };
 
 /*
