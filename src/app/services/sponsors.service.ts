4b65b889579c7445dc8fe8ddf7dce24de85fea62
Linus <vitocorleone77@gmail.com>
2021-07-25 22:03:56 +0200

    sponsors order

--- src/app/services/sponsors.service.ts
+++ src/app/services/sponsors.service.ts
@@ -38,8 +38,21 @@ export class SponsorsService {
 				map(
 					(result) => {
 						return event !== ""
-							? result.data.sponsors.filter(sponsor => sponsor.events.find(eventOccurance => eventOccurance.event === event))
-							: result.data.sponsors;
+							? result.data.sponsors
+								.filter(sponsor => sponsor.events
+									.find(eventOccurance => eventOccurance.event === event))
+								.sort((o1, o2) =>
+									o2.events.find(eventOccurance => eventOccurance.event === event).share -
+									o1.events.find(eventOccurance => eventOccurance.event === event).share)
+							: result.data.sponsors
+							.filter(sponsor => sponsor.events
+								.find(eventOccurance => eventOccurance.event !== ''))
+							.sort((o1, o2) =>
+							(o2.events.reduce(function(prev, current) {
+								return (prev.share > current.share) ? prev : current}).share) -
+							(o1.events.reduce(function(prev, current) {
+								return (prev.share > current.share) ? prev : current}).share)
+						)
 					}
 				)
 			)
