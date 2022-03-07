ebb8ca660735d3e946c535beb81d4cd4fd30a54c
Linus <vitocorleone77@gmail.com>
2022-03-07 22:56:45 +0100

    gitflow-feature-stash: fn-token-expired

--- src/app/realm.ts
+++ src/app/realm.ts
@@ -1,17 +1,19 @@
 import * as Realm from 'realm-web';
 import {environment} from "../environments/environment";
+import jwtDecode from "jwt-decode";
 
 const graphqlUrl = `https://realm.mongodb.com/api/client/v2.0/app/${environment.APP_ID_REALM}/graphql`;
 
 // Connect to your MongoDB Realm app
 const app = new Realm.App(environment.APP_ID_REALM);
 
+
 // Get a valid Realm user access token to authenticate requests
 async function getValidAccessToken(): Promise<string> {
 
 	console.log('app.currentUser: ' + JSON.stringify(app.currentUser));
 
-	if (!app.currentUser)
+	if (!app.currentUser || tokenExpired(app.currentUser))
 		// If no user is logged in, log in an anonymous user
 	{
 		await app.logIn(Realm.Credentials.anonymous()).then(o => {
@@ -38,6 +40,22 @@ async function getValidAccessToken(): Promise<string> {
 	return app.currentUser.accessToken;
 }
 
+function tokenExpired(currentUser:any):boolean{
+	if (currentUser?.accessToken) {
+		console.log('token found');
+		const decoded = jwtDecode(currentUser.accessToken);
+		const expDate = +decoded['exp'];
+		if (expDate < Date.now() / 1000) {
+			localStorage.removeItem('token')
+			currentUser.accessToken = null;
+			console.log('expired');
+			return false;
+		}else{
+			console.log('valid');
+			return true;
+		}
+	}
+}
 export {
 	graphqlUrl,
 	getValidAccessToken
