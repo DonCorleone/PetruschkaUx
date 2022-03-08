959566d867514801147f55d94b9a0ec05b221a12
Don Corleone <vitocorleone77@gmail.com>
2022-03-08 17:05:10 +0100

    Update realm.ts
    avoid crash

--- src/app/realm.ts
+++ src/app/realm.ts
@@ -11,7 +11,7 @@ const app = new Realm.App(environment.APP_ID_REALM);
 // Get a valid Realm user access token to authenticate requests
 async function getValidAccessToken(): Promise<string> {
 
-	console.log('app.currentUser: ' + JSON.stringify(app.currentUser));
+	console.log('app.currentUser: ' + JSON.stringify(app));
 
 	if (!app.currentUser || tokenExpired(app.currentUser))
 		// If no user is logged in, log in an anonymous user
