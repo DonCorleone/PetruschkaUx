058ada17c939a8f112cf7792aa4bf391739d1370
Don Corleone <vitocorleone77@gmail.com>
2022-03-20 13:08:00 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,1 @@
-function getApiBasePath(): string {
-	return (window as any).config.API_BASE_PATH  || 'default-url';
-}
-export const environment = {
-	production: false,
-	API_KEY_IMAGE4IO: "4Yy9ZWYNl/HKYOwSIMI9LQ==",
-	API_SECRET_IMAGE4IO: "FQw/fSWJTedUN36rXBBxAXsG+DKuM+Sz3xvk9m+IhRU=",
-	API_URL_IMAGE4IO: "https://api.image4.io/v0.1",
-	APP_ID_REALM: "application-0-cgymr",
-	NODE_VERSION: "16"
-};
+
