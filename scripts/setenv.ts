429bf13159c1a4b6886f4663f1c8ca833b6e375c
Linus <linus@wieland.lu>
2022-07-26 22:50:14 +0200

    prod

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,30 +1,31 @@
 const { writeFile } = require('fs');
+const { argv } = require('yargs'); // read environment variables from .env file
+require('dotenv').config(); // read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+const targetPath = `./src/environments/environment.custom.ts`; // we have access to our environment variables
+// in the process.env object thanks to dotenv
 
-// Your environment.custom.ts file. Will be ignored by git.
-const targetPath = './src/environments/environment.custom.ts';
-
-// Load dotenv to work with process.env
-require('dotenv').config();
-
-// environment.ts file structure
-
-const envConfigFile = `
-function getApiBasePath(): string {
-	return (window as any).config.API_BASE_PATH  || 'default-url';
-}
-export const environment = {
+const environmentFileContent = isProduction
+	? `export const environment = {
+   production: true,
+   APP_ID_REALM: "${process.env.APP_ID_REALM}",
+   NODE_VERSION: "${process.env.NODE_VERSION}",
+   SITE_ID: "${process.env?.['SITE_ID']}",
+   API_KEY_NETLIFY: "${process.env?.['API_KEY_NETLIFY']}",
+   URL: "${process.env?.['URL']}",
+};`
+	: `export const environment = {
    production: false,
    APP_ID_REALM: "${process.env.APP_ID_REALM}",
    NODE_VERSION: "${process.env.NODE_VERSION}",
    SITE_ID: "${process.env?.['SITE_ID']}",
    API_KEY_NETLIFY: "${process.env?.['API_KEY_NETLIFY']}",
    URL: "${process.env?.['URL']}",
-};
-`; // write the content to the respective file
-writeFile(targetPath, envConfigFile, function (err) {
-  if (err) {
-    throw console.error(err);
-  } else {
-    console.log('Using custom environment');
-  }
+};`; // write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err: any) {
+	if (err) {
+		console.log(err);
+	}
+	console.log(`Wrote variables to ${targetPath}`);
 });
