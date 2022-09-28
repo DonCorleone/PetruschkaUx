4c2aabe99d15ac3bb75bf6677d567112d7186ca3
Linus <linus@wieland.lu>
2022-09-28 19:15:53 +0200

    locations

--- src/app/components/location/location-item/location-item.component.ts
+++ src/app/components/location/location-item/location-item.component.ts
@@ -1,31 +1,28 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
-import {Observable} from 'rxjs';
-import {EventLocation} from 'src/app/models/location.models';
-import {LocationsService} from 'src/app/services/locations.service';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { EventLocation, LocationsService } from 'src/app/services/locations.service';
 
 @Component({
-	selector: 'app-location-item',
-	templateUrl: './location-item.component.html',
-	styleUrls: ['./location-item.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-location-item',
+  templateUrl: './location-item.component.html',
+  styleUrls: ['./location-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LocationItemComponent {
+  eventLocation$: Observable<EventLocation>;
+  @Input() set eventLocationName(value: string) {
+    this.eventLocation$ = this.locationsService.GetEventLocation(value);
+  }
 
-	eventLocation$: Observable<EventLocation>;
-	@Input() set eventLocationName(value :string){
-		this.eventLocation$ = this.locationsService.GetEventLocation(value);
-	}
+  constructor(private locationsService: LocationsService, private sanitizer: DomSanitizer) {}
 
-	constructor(private locationsService: LocationsService, private sanitizer: DomSanitizer) {
-	}
+  GetImageUrl(name: string): string {
+    let imageUrl = 'https://petruschka.netlify.app/' + 'assets/images/members/' + encodeURIComponent(name) + '.jpg';
+    return 'https://images.weserv.nl/?url=' + imageUrl + '&w583&h=738';
+  }
 
-	GetImageUrl(name: string): string {
-		let imageUrl = 'https://petruschka.netlify.app/' + 'assets/images/members/' + encodeURIComponent(name) + '.jpg';
-		return "https://images.weserv.nl/?url=" + imageUrl + "&w583&h=738";
-	}
-
-	transformHtml(htmlTextWithStyle) {
-		return this.sanitizer.bypassSecurityTrustHtml(htmlTextWithStyle);
-	}
+  transformHtml(htmlTextWithStyle) {
+    return this.sanitizer.bypassSecurityTrustHtml(htmlTextWithStyle);
+  }
 }
--- src/app/models/location.models.ts
+++
@@ -1,9 +0,0 @@
-export interface EventLocation {
-	city: string;
-	directions: string;
-	info: string;
-	name: string;
-	postalCode: string;
-	street: string;
-}
-
