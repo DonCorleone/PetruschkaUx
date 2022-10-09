7bfb14b21cdf7d6634ad2e5b6a5c3e2512e8df40
Linus <linus@wieland.lu>
2022-10-09 22:07:19 +0200

    postman2

--- stuff/ef_backup/postman.sh
+++ stuff/ef_backup/postman.sh
@@ -1,21 +1,24 @@
-GET /api/web/events/6983911679974081447/categories/ HTTP/1.1
-Referrer: https://eventfrog.ch/de/cockpitv3/events/6983911679974081447/categories
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0
-X-Content-Type-Options: nosniff
-x-frame-options: SAMEORIGIN
-x-xss-protection: 1; mode=block; report-uri='https://eventfrog.report-uri.com/r/d/xss/enforce'
-Postman-Token: a70cccb6-29eb-4b53-9eb0-0909c2b54e15
+GET /api/web/events/6983911679974081447/categories HTTP/1.1
+Authorization: Basic cGV0cnVzY2hrYUBraW5kZXJrdWx0dXIuY2g6cmVxamk5LXhlcXpvai1waUpnZW4=
+User-Agent: PostmanRuntime/7.29.2
+Accept: */*
+Postman-Token: f3cd27e2-076f-45b4-a94a-9ca825a2377b
+Host: eventfrog.ch
+Accept-Encoding: gzip, deflate, br
+Connection: keep-alive
 Cookie: JSESSIONID=2C9CFFCE52231EBB9C0ACC55B8A3184F
  
 HTTP/1.1 404 404
 Server: Apache
 X-Content-Security-Policy:
-Content-Type: text/html;charset=utf-8
+Cache-Control: private, no-store, no-cache, max-age=0, must-revalidate
+Content-Type: text/html;charset=UTF-8
 strict-transport-security: max-age=31536000; includeSubDomains
 Content-Security-Policy:
-Date: Sun, 09 Oct 2022 19:58:36 GMT
+Date: Sun, 09 Oct 2022 20:04:39 GMT
+Keep-Alive: timeout=2, max=100
 X-XSS-Protection: 1; mode=block; report-uri='https://eventfrog.report-uri.com/r/d/xss/enforce'
-Content-Language: en
+Transfer-Encoding: chunked
 X-Content-Type-Options: nosniff
 Upgrade: h2,h2c
 Connection: Keep-Alive
@@ -24,6 +27,3 @@ Expect-CT: max-age=0, report-uri='https://eventfrog.report-uri.com/r/d/ct/report
 X-WebKit-CSP:
 Referrer-Policy: strict-origin-when-cross-origin
 Permissions-Policy: microphone=(), camera=()
-Content-Length: 431
- 
-<!doctype html><html lang="en"><head><title>HTTP Status 404 – Not Found</title><style type="text/css">body {font-family:Tahoma,Arial,sans-serif;} h1, h2, h3, b {color:white;background-color:#525D76;} h1 {font-size:22px;} h2 {font-size:16px;} h3 {font-size:14px;} p {font-size:12px;} a {color:black;} .line {height:1px;background-color:#525D76;border:none;}</style></head><body><h1>HTTP Status 404 – Not Found</h1></body></html>
