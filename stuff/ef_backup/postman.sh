5ee40ad7895b66f7ecd122cdd5f76eb3ba5761bf
Linus <linus@wieland.lu>
2022-10-09 22:02:11 +0200

    git

---
+++ stuff/ef_backup/postman.sh
@@ -0,0 +1,29 @@
+GET /api/web/events/6983911679974081447/categories/ HTTP/1.1
+Referrer: https://eventfrog.ch/de/cockpitv3/events/6983911679974081447/categories
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0
+X-Content-Type-Options: nosniff
+x-frame-options: SAMEORIGIN
+x-xss-protection: 1; mode=block; report-uri='https://eventfrog.report-uri.com/r/d/xss/enforce'
+Postman-Token: a70cccb6-29eb-4b53-9eb0-0909c2b54e15
+Cookie: JSESSIONID=2C9CFFCE52231EBB9C0ACC55B8A3184F
+ 
+HTTP/1.1 404 404
+Server: Apache
+X-Content-Security-Policy:
+Content-Type: text/html;charset=utf-8
+strict-transport-security: max-age=31536000; includeSubDomains
+Content-Security-Policy:
+Date: Sun, 09 Oct 2022 19:58:36 GMT
+X-XSS-Protection: 1; mode=block; report-uri='https://eventfrog.report-uri.com/r/d/xss/enforce'
+Content-Language: en
+X-Content-Type-Options: nosniff
+Upgrade: h2,h2c
+Connection: Keep-Alive
+X-Frame-Options: SAMEORIGIN
+Expect-CT: max-age=0, report-uri='https://eventfrog.report-uri.com/r/d/ct/reportOnly'
+X-WebKit-CSP:
+Referrer-Policy: strict-origin-when-cross-origin
+Permissions-Policy: microphone=(), camera=()
+Content-Length: 431
+ 
+<!doctype html><html lang="en"><head><title>HTTP Status 404 – Not Found</title><style type="text/css">body {font-family:Tahoma,Arial,sans-serif;} h1, h2, h3, b {color:white;background-color:#525D76;} h1 {font-size:22px;} h2 {font-size:16px;} h3 {font-size:14px;} p {font-size:12px;} a {color:black;} .line {height:1px;background-color:#525D76;border:none;}</style></head><body><h1>HTTP Status 404 – Not Found</h1></body></html>
