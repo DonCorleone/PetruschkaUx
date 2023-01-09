a1609429a810a300967d0beca83ecd2be4b6942c
Linus <linus@wieland.lu>
2023-01-09 22:36:46 +0100

    enable scrolling in theory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy', anchorScrolling:'enabled' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
