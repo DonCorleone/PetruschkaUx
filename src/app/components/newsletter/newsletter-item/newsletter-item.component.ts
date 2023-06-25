6fe770fe5c888194f34a684776eb7a0778285f6e
DonCorleone <linus@wieland.click>
2023-06-25 18:51:01 +0200

    ts success

--- src/app/components/newsletter/newsletter-item/newsletter-item.component.ts
+++ src/app/components/newsletter/newsletter-item/newsletter-item.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-newsletter-item',
@@ -6,4 +7,20 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./newsletter-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NewsletterItemComponent {}
+export class NewsletterItemComponent {
+  constructor(private router: Router) { }
+
+  submitHandler(event: Event): void {
+    event.preventDefault();
+    const myForm = document.getElementById("contact-form") as HTMLFormElement;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(<any>formData).toString(),
+    })
+    .then(() => this.router.navigate(['/success']))
+    .catch((error) => alert(error));
+  }
+}
