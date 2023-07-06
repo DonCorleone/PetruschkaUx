051f7e7979ad78b2b53daebddb93939987b2c9fa
DonCorleone <linus@wieland.click>
2023-07-06 21:47:32 +0200

    480

--- src/app/components/sponsors/sponsors.component.ts
+++ src/app/components/sponsors/sponsors.component.ts
@@ -28,7 +28,7 @@ export class SponsorsComponent implements OnInit {
   getImagePath(sponsor: Sponsor): string {
 		// calculate height of image based on share
 		const share = sponsor.events.find(p => p.event == this.eventKey).share;
-		const heightInt = Math.round(share * 500);
+		const heightInt = Math.round(share * 480);
 		return `https://www.petruschka.ch/assets/images/sponsoren/sponsors_${sponsor.image}?nf_resize=fit&h=${heightInt}`;
   }
 }
