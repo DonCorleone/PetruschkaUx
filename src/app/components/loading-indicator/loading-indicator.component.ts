e70c77b19a16b7b0e7bfba283d128f1c23f8bac2
linus <linus@wieland.click>
2023-09-07 22:46:10 +0200

    core

--- src/app/components/loading-indicator/loading-indicator.component.ts
+++ src/app/components/loading-indicator/loading-indicator.component.ts
@@ -1,10 +1,24 @@
 import {Component, Input} from '@angular/core';
 import {LoadingIndicatorService} from "../../services/loading-indicator.service";
+import {animate, keyframes, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-loading-indicator',
   templateUrl: './loading-indicator.component.html',
-  styleUrls: ['./loading-indicator.component.scss']
+  styleUrls: ['./loading-indicator.component.scss'],
+	animations: [
+		trigger('loadingRotate', [
+			transition('void => *', [
+				animate('1s', keyframes([
+					style({ transform: 'rotate(0deg) scaleX(1)', offset: 0 }),
+					style({ transform: 'rotate(90deg) scaleX(0.3)', offset: 0.25 }),
+					style({ transform: 'rotate(180deg) scaleX(1)', offset: 0.5 }),
+					style({ transform: 'rotate(270deg) scaleX(0.3)', offset: 0.75 }),
+					style({ transform: 'rotate(360deg) scaleX(1)', offset: 1 })
+				]))
+			])
+		])
+	]
 })
 export class LoadingIndicatorComponent{
 
--- src/app/components/promo/promo-carousel/promo-carousel.component.ts
+++ src/app/components/promo/promo-carousel/promo-carousel.component.ts
@@ -1,12 +1,22 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
 import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+import {animate, keyframes, style, transition, trigger} from "@angular/animations";
 
 @Component({
 	selector: 'app-promo-carousel',
 	templateUrl: './promo-carousel.component.html',
 	styleUrls: ['./promo-carousel.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	encapsulation: ViewEncapsulation.None
+	animations: [
+		trigger('zoom', [
+			transition('void => *', [
+				animate('0.5s', keyframes([
+					style({ transform: 'scale(1.4, 1.4)', offset: 0 }),
+					style({ transform: 'scale(1, 1)', offset: 1 })
+				]))
+			])
+		])
+	]
 })
 export class PromoCarouselComponent {
 
