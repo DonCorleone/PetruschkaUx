842aa39e8f339db0d643333ef73a2a2f8ca2937b
linus <linus@wieland.click>
2023-10-10 00:08:57 +0200

    fix animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,8 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { HttpErrorInterceptor } from './services/http-error-interceptor.service';
 import { provideRouter } from '@angular/router';
 import { ROUTES } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { LoadingIndicatorInterceptor } from './interceptors/loading-indicator.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,7 +13,13 @@ export const appConfig: ApplicationConfig = {
       useClass: HttpErrorInterceptor,
       multi: true,
     },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingIndicatorInterceptor,
+      multi: true,
+    },
     importProvidersFrom(HttpClientModule),
     provideRouter(ROUTES),
+    provideAnimations()
   ],
 };
