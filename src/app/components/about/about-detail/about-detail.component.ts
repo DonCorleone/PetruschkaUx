0191fc67f71ff443ffd3134a2f234c7a62320200
linus <linus@wieland.click>
2023-11-19 20:15:24 +0100

    running dialog

--- src/app/components/about/about-detail/about-detail.component.ts
+++ src/app/components/about/about-detail/about-detail.component.ts
@@ -1,8 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AboutModalComponent } from '../about-modal/about-modal.component';
 import { Staff } from '../../../models/staff.models';
 import { CommonModule } from '@angular/common';
+import { StaffService } from '../../../services/staff.service';
+import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-about-detail',
@@ -12,14 +15,12 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutDetailComponent {
-  @Input() staff: Staff;
-  @Input() isModal: boolean;
+export class AboutDetailComponent implements OnInit {
+  @Input() staffName: string;
+  staff$: Observable<Staff>;
+  constructor(private staffService: StaffService) {}
 
-  constructor(private modalService: NgbModal) {}
-
-  openStaff(staffName: string) {
-    const modalRef = this.modalService.open(AboutModalComponent, { size: 'sm' });
-    modalRef.componentInstance.staffName = staffName;
+  ngOnInit(): void {
+    this.staff$ = this.staffService.GetStaff(this.staffName);
   }
 }
