518e0943bcabcebde66bccb5cb321030e1d751c7
linus <linus@wieland.click>
2023-11-26 23:14:42 +0100

    nice refactor

--- src/app/components/music/music-detail/music-detail.component.ts
+++ src/app/components/music/music-detail/music-detail.component.ts
@@ -1,62 +1,66 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { EventDetail, TicketTypeInfo } from 'src/app/models/event.models';
 import { EventService } from 'src/app/services/event.service';
 import { InfoModalComponent } from '../../info/info-modal/info-modal.component';
 import { AboutModalComponent } from '../../about/about-modal/about-modal.component';
 import { StaffService } from '../../../services/staff.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { take } from 'rxjs/operators';
+import { Job } from '../../../models/staff.models';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-music-detail',
   templateUrl: './music-detail.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MusicDetailComponent implements OnInit {
   @Input() eventDetailId: number;
 
-  eventDetail$: Observable<EventDetail>;
+  eventDetail: EventDetail;
+  shortDesc: string;
+  ticketTypeInfo: TicketTypeInfo;
+  artistsArray: Job[];
+  eventName: string;
+  imagePath: string;
 
-  GetTicketTypeInfoFromEventDetail(eventDetail: EventDetail) {
-    return EventService.GetTicketTypeInfoFromEventDetail(eventDetail, 'CD');
-  }
-
-  ArtistsArray(ticketTypeInfo: TicketTypeInfo) {
-    return StaffService.GetStaffLinks(ticketTypeInfo.description);
-  }
-
-  eventName(eventDetail: EventDetail): string {
-    return (eventDetail && eventDetail.eventInfos?.filter((p) => p.languageId === 0))?.length > 0
-      ? eventDetail.eventInfos?.filter((p) => p.languageId === 0)[0].name
-      : null;
-  }
-
-  imagePath(ticketTypeInfo: TicketTypeInfo): string {
-
-		const imageUrl = ticketTypeInfo?.imageUrl;
+  getImagePath(ticketTypeInfo: TicketTypeInfo): string {
+    const imageUrl = ticketTypeInfo?.imageUrl;
     return imageUrl ? imageUrl + '?nf_resize=fit&w=900' : '';
   }
-
-  shortDesc(eventDetail: EventDetail): string {
-    return (eventDetail && eventDetail.eventInfos?.filter((p) => p.languageId === 0))?.length > 0
-      ? eventDetail.eventInfos?.filter((p) => p.languageId === 0)[0].shortDescription
-      : null;
-  }
-
-  constructor(private modalService: NgbModal, private staffService: StaffService, private eventService: EventService) {}
+  constructor(
+    private modalService: NgbModal,
+    private eventService: EventService,
+    private changeDetectionRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.eventDetail$ = this.eventService.GetEventDetail(this.eventDetailId);
+    this.eventService
+      .GetEventDetail(this.eventDetailId)
+      .pipe(take(1))
+      .subscribe((eventDetail) => {
+        this.eventDetail = eventDetail;
+        const eventDetailEventInfo = eventDetail.eventInfos?.filter((p) => p.languageId === 0)?.pop();
+        this.shortDesc = eventDetailEventInfo?.shortDescription;
+        this.eventName = eventDetailEventInfo?.name;
+
+        const ticketTypeInfo = EventService.GetTicketTypeInfoFromEventDetail(eventDetail, 'CD');
+        this.ticketTypeInfo = ticketTypeInfo;
+        this.artistsArray = StaffService.GetStaffLinks(ticketTypeInfo.description);
+        this.imagePath = this.getImagePath(ticketTypeInfo);
+
+        this.changeDetectionRef.markForCheck();
+      });
   }
 
   openInfo(): void {
     const modalRef = this.modalService.open(InfoModalComponent, { size: 'lg' });
     modalRef.componentInstance.eventDetailId = this.eventDetailId;
     modalRef.componentInstance.usage = 'CD'; // else case
-    modalRef.componentInstance.eventDetail$ = this.eventDetail$;
+    modalRef.componentInstance.eventDetail$ = of(this.eventDetail);
   }
 
   openStaff(staffName: string) {
--- src/app/components/music/music-item/music-item.component.ts
+++ src/app/components/music/music-item/music-item.component.ts
@@ -1,39 +1,42 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {EventDetail, EventDetailEventInfo, TicketTypeInfo} from 'src/app/models/event.models';
-import {MusicModalComponent} from '../music-modal/music-modal.component';
-import {EventService} from "../../../services/event.service";
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EventDetail, EventDetailEventInfo, TicketTypeInfo } from 'src/app/models/event.models';
+import { MusicModalComponent } from '../music-modal/music-modal.component';
+import { EventService } from '../../../services/event.service';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
-	selector: 'app-music-item',
-	templateUrl: './music-item.component.html',
-	styleUrls: ['./music-item.component.scss'],
-	standalone: true,
-	imports: [CommonModule],
-	changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-music-item',
+  templateUrl: './music-item.component.html',
+  styleUrls: ['./music-item.component.scss'],
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class 	MusicItemComponent implements OnInit{
-	@Input() eventDetail: EventDetail;
-	@Input() usage: string;
+export class MusicItemComponent implements OnInit {
+  @Input() eventDetail: EventDetail;
+  @Input() usage: string;
 
-	eventInfo: EventDetailEventInfo | undefined;
-	comingSoon: boolean;
-	imageUrl: string;
+  eventInfo: EventDetailEventInfo | undefined;
+  comingSoon: boolean;
+  imageUrl: string;
+  imageDimension: number;
 
-	constructor(private modalService: NgbModal) {
-	}
+  constructor(private modalService: NgbModal) {
+    this.imageDimension = 300;
+  }
 
-	ngOnInit(): void {
-		this.eventInfo = this.eventDetail?.eventInfos?.find((p) => p.languageId === 0)
-		this.comingSoon = this.eventDetail.start > new Date();
-		this.imageUrl = this.GetImageUrl(this.eventDetail);
-	}
-	openDetail(): void {
-		const modalRef = this.modalService.open(MusicModalComponent, { size:'lg' });
-		modalRef.componentInstance.eventDetailId = this.eventDetail?._id;
-	}
-	GetImageUrl(eventDetail: EventDetail):string{
-		return EventService.GetTicketTypeInfoFromEventDetail(eventDetail, 'CD')?.imageUrl;
-	}
+  ngOnInit(): void {
+    this.eventInfo = this.eventDetail?.eventInfos?.find((p) => p.languageId === 0);
+    this.comingSoon = this.eventDetail.start > new Date();
+    this.imageUrl = this.getImageUrl(this.eventDetail);
+  }
+  openDetail(): void {
+    const modalRef = this.modalService.open(MusicModalComponent, { size: 'lg' });
+    modalRef.componentInstance.eventDetailId = this.eventDetail?._id;
+  }
+  private getImageUrl(eventDetail: EventDetail): string {
+    const imageUrl = EventService.GetTicketTypeInfoFromEventDetail(eventDetail, 'CD')?.imageUrl;
+    return `${imageUrl}?nf_resize=smartcrop&w=${this.imageDimension}&h=${this.imageDimension}`;
+  }
 }
